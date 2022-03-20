2fca261f1958b115d4dc4c8ea7a43a42165b8631
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2022-03-19 23:16:35 -0700

    Update ContactForm.jsx

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/26a14650-f678-4244-89c9-ae025c3d70b8/deploy-status)](https://app.netlify.com/sites/jeremy-nguyen/deploys)
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,47 +4,37 @@ import { Form, Button, Col, FloatingLabel } from "react-bootstrap";
 function ContactForm() {
   return (
     <Col lg={4}>
-      <form method="POST" name="contact" data-netlify="true">
+      <form method="POST" name="contact">
         <input type="hidden" name="form-name" value="contact" />
-        <Form.Group controlId="name" size="sm">
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Name"
-            className="mb-4"
-          >
-            <Form.Control
-              as="input"
-              type="text"
-              name="name"
-              placeholder="John Doe"
-            />
-          </FloatingLabel>
-        </Form.Group>
-        <Form.Group className="mb-4" controlId="email">
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Email address"
-            className="mb-3"
-          >
-            <Form.Control
-              as="input"
-              type="email"
-              name="email"
-              placeholder="name@example.com"
-            />
-          </FloatingLabel>
-        </Form.Group>
-        <Form.Group className="mb-4" controlId="message">
-          <FloatingLabel controlId="floatingTextarea" label="Message">
-            <Form.Control
-              as="textarea"
-              name="message"
-              placeholder="Leave a message here"
-              style={{ height: "100px" }}
-            />
-          </FloatingLabel>
-        </Form.Group>
-        <Button variant="primary" className="w-100 py-2" type="submit">
+        <FloatingLabel controlId="floatingInput" label="Name" className="mb-4">
+          <Form.Control
+            as="input"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+          />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Email address"
+          className="mb-4"
+        >
+          <Form.Control
+            as="input"
+            type="email"
+            name="email"
+            placeholder="name@example.com"
+          />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingTextarea" label="Message">
+          <Form.Control
+            as="textarea"
+            name="message"
+            placeholder="Leave a message here"
+            style={{ height: "100px" }}
+          />
+        </FloatingLabel>
+        <Button variant="primary" className="w-100 py-2 mt-4" type="submit">
           Get in touch
         </Button>
       </form>
