f96be43fe56920387e2efe19f38f10d7649d04ba
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2022-03-22 23:37:48 -0700

    Update AboutCards.jsx

--- src/components/AboutCards.jsx
+++ src/components/AboutCards.jsx
@@ -2,24 +2,35 @@ import React from "react";
 import { Card, Row, Col } from "react-bootstrap";
 import { IconContext } from "react-icons";
 
-import styles from "./AboutCards.module.css";
-
 function AboutCards({ icons, headings, descriptions }) {
+  const cardStyle = {
+    width: "5rem",
+    height: "5rem",
+    borderRadius: "50%",
+    backgroundColor: "#0d6efd",
+    display: "grid",
+    placeItems: "center",
+    margin: "0 auto"
+  };
   return (
-    <Row xs={1} md={3} className="g-4">
+    <Row xs={1} md={3} className="g-4 d-flex justify-content-around" >
       {icons.map((icon, index) => (
-        <Col>
-          <Card key={index} className="mb-3 px-2 d-flex h-100">
-            <Card.Body>
-              <div className={styles.icon}>
-                <IconContext.Provider value={{ color: "white" }}>
-                  {React.createElement(icon, { size: 24 })}
-                </IconContext.Provider>
+        <Col key={index} style={{maxWidth:"45ch"}}>
+          <Card key={index} className="mb-3 p-3 d-flex h-100">
+            <Card.Body className="d-flex flex-column justify-content-between">
+              <div>
+                <div style={cardStyle}>
+                  <IconContext.Provider value={{ color: "white" }}>
+                    {React.createElement(icon, { size: 30, key: index })}
+                  </IconContext.Provider>
+                </div>
+                <h3 className="d-flex justify-content-center mt-3 text-dark fs-3">
+                  {headings[index]}
+                </h3>
               </div>
-              <h3 className="d-flex justify-content-center my-3 text-dark">
-                {headings[index]}
-              </h3>
-              <Card.Text class="text-dark">{descriptions[index]}</Card.Text>
+              <Card.Text className="text-dark mb-0">
+                {descriptions[index]}
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
